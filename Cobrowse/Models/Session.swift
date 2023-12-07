8c2bf296aa88752f0573968faa6897b6185096ef
Ste Prescott <ste@cobrowse.io>
2023-12-07 15:49:25 +0000

    Add redaction by default example

--- Cobrowse/Models/Session.swift
+++ Cobrowse/Models/Session.swift
@@ -11,11 +11,22 @@ class Session: NSObject, CobrowseIODelegate {
     
     @Published var current: CBIOSession?
     
+    let isRedactionByDefaultEnabled = true
+    
     func cobrowseSessionDidUpdate(_ session: CBIOSession) {
         current = session
     }
     
     func cobrowseSessionDidEnd(_ session: CBIOSession) {
         current = nil
+    }
+    
+    func cobrowseRedactedViews(for vc: UIViewController) -> [UIView] {
+        
+        guard isRedactionByDefaultEnabled,
+              let keyWindow = UIWindow.keyWindow
+            else { return [] }
+        
+        return keyWindow.rootViews
     }
 }
